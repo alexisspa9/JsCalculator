0082d4f893a0e411c419c22aacba505c80445750
cnagadya <ncritah@gmail.com>
2019-02-01 11:19:16 +0300

    Keyboard support

--- calculator.js
+++ calculator.js
@@ -1,63 +1,58 @@
 const screen = document.getElementById("screen");
+// const pressedKey =
+const validOps = ["+", "-", "*", "/"];
+
+document.addEventListener("keydown", event => {
+  if (event.key >= 0 && event.key <= 9) {
+    addToScreen(event.key);
+  } else if (validOps.includes(event.key)) {
+    operations(event.key);
+  } else if (event.key === ".") {
+    addDot();
+  } else if (event.key === "=") {
+    equals();
+  }
+});
 
 let dotPressed = false;
 
 function addToScreen(n) {
-	screen.value += n;
+  screen.value += n;
 }
 
-
 function clearScreen() {
-	screen.value = "";
+  screen.value = "";
 }
 
 function addDot() {
-	if (dotPressed || !parseInt(screen.value.charAt( screen.value.length-1)) || screen.value.charAt( screen.value.length-1) === '.') {
-		return;
-	}
-	screen.value += '.';
-	dotPressed = true;
+  if (
+    dotPressed ||
+    !parseInt(screen.value.charAt(screen.value.length - 1)) ||
+    screen.value.charAt(screen.value.length - 1) === "."
+  ) {
+    return;
+  }
+  screen.value += ".";
+  dotPressed = true;
 }
 
 function backspace() {
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-	screen.value = screen.value.slice(0, -1)
-	} else {
-	screen.value = screen.value.slice(0, -3)
-	}
+  if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
+    screen.value = screen.value.slice(0, -1);
+  } else {
+    screen.value = screen.value.slice(0, -3);
+  }
 }
-function add() {
-	dotPressed = false;
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-		screen.value += " + ";
-	}
-}
-
-function subtract() {
-	dotPressed = false;
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-	screen.value += " - ";
-	}
-}
-
-function multiply() {
-	dotPressed = false;
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-	screen.value += " * ";
-	}
-}
-
-function divide() {
-	dotPressed = false;
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-	screen.value += " / ";
-	}
+function operations(op) {
+  dotPressed = false;
+  if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
+    screen.value += ` ${op} `;
+  }
 }
 
 function equals() {
-	dotPressed = false;
-	if(parseInt(screen.value.charAt( screen.value.length-1)) >= 0) {
-	screen.value = eval(screen.value);
-	}
+  dotPressed = false;
+  if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
+    screen.value = eval(screen.value);
+  }
 }
-
