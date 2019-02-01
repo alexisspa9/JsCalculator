71fb000edfde956dcd2e332d5f11228f051f9f3e
alexisspa9 <alexisspa9@gmail.com>
2019-02-01 12:11:06 +0200

    minor fixes

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
+// Screen element
 const screen = document.getElementById("screen");
 // const pressedKey =
 const validOps = ["+", "-", "*", "/"];
+// helper variable for "."
+let dotPressed = false;
 
+
+// Add Keyboard support
 document.addEventListener("keydown", event => {
   if (event.key >= 0 && event.key <= 9) {
     addToScreen(event.key);
@@ -11,19 +16,34 @@ document.addEventListener("keydown", event => {
     addDot();
   } else if (event.key === "=") {
     equals();
+  } else if (event.key === "Backspace") {
+  	backspace();
+  } else if (event.keyCode === 13) {
+  	equals();
   }
 });
 
-let dotPressed = false;
 
+// adds n value to screen element
 function addToScreen(n) {
   screen.value += n;
 }
 
+// clears the screen element
 function clearScreen() {
   screen.value = "";
 }
 
+// removes last pressed from screen element
+function backspace() {
+  if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
+    screen.value = screen.value.slice(0, -1);
+  } else {
+    screen.value = screen.value.slice(0, -3);
+  }
+}
+
+// adds "." to number
 function addDot() {
   if (
     dotPressed ||
@@ -36,13 +56,8 @@ function addDot() {
   dotPressed = true;
 }
 
-function backspace() {
-  if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
-    screen.value = screen.value.slice(0, -1);
-  } else {
-    screen.value = screen.value.slice(0, -3);
-  }
-}
+
+// performs op operation
 function operations(op) {
   dotPressed = false;
   if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
@@ -50,6 +65,8 @@ function operations(op) {
   }
 }
 
+
+// evaluates the solution of the screen element value
 function equals() {
   dotPressed = false;
   if (parseInt(screen.value.charAt(screen.value.length - 1)) >= 0) {
